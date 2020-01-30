6237af74988a0820f16e8630af4ee634673040b1
koyag398398 <udkudkudk.udk@gmail.com>
2020-01-30 10:22:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a4/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.a4
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        button.setOnClickListener {
+            MyAsyncTask(this).execute("Param1")
+        }
+
+    }
+}
